3955ef6a67468b47aff70881fb481a4379da16cf
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-13 21:08:32 +0300

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Concat Function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A function that adds two numbers
+
+    Arguments:
+        str1 (str): The first string
+        str2 (str): The second string
+
+    Return:
+        str: The concatenation of two strings
+    """
+    return str1 + str2
