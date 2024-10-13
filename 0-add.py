5c85f6555d1d8360ca89f83593098a56a12570e4
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-13 21:00:08 +0300

    Task 0

---
+++ 0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Add Function"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    A function that adds two numbers
+
+    Arguments:
+        a (float): The first number
+        b (float): The second number
+
+    Return:
+        float: The sum of two numbers
+    """
+    return a + b
