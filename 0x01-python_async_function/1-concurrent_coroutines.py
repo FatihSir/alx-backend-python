b78c457a81aabfab0fee6f78f9b0e1f4a0f860e2
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-15 00:42:31 +0300

    Task 2

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,17 +3,22 @@
 
 
 import asyncio
-import time
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    async routine called wait_n that takes in 2 int arguments (in this order):
-    n and max_delay. You will spawn wait_random n times with the
-    specified max_delay.
+    Runs wait_random asynchronously n times with a specified max_delay and
+    returns the list of completed delays in the order they finish.
+
+    Args:
+        n (int): The number of times to run wait_random.
+        max_delay (int): The maximum delay for wait_random.
+
+    Returns:
+        List[float]: A list of delays, sorted in the order of completion.
     """
-    start = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    end = time.perf_counter()
-    return (end - start) / n
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = [await task for task in asyncio.as_completed(tasks)]
+    return delays
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Measure the runtime """
+
+
+import asyncio
+import time
+wait_random = __import__('1-concurrent_coroutines').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+        Spawns wait_random n times with a specified max_delay,
+        returns the list of delays in the order they are completed.
+
+        Args:
+            n (int): Number of times to run wait_random
+            max_delay (int): Maximum delay to pass to wait_random
+
+        Returns:
+            List[float]: List of delays in the order of completion
+    """
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
+    return (end - start) / n
