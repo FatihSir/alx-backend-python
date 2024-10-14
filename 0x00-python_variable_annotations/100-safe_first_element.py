0b454c0892bdc800e3f23dc72300178468d56616
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-14 16:27:46 +0300

    Documentation improvement

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,7 @@ from typing import Union, Sequence, Any
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """The function is intended to be used to practice duck-typed annotation"""
     if lst:
         return lst[0]
     else:
