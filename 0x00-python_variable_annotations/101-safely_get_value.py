51c0f98fc41e0a42bd4b7971a485e685a25b5ccf
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-14 16:51:44 +0300

    Task 101 improvement

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,8 +6,8 @@ from typing import Union, Any, Mapping, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, Any] = None) \
-        -> Union[T, Any]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) \
+        -> Union[Any, T]:
     """The function is intended to be used to practice duck-typed annotation"""
     if key in dct:
         return dct[key]
